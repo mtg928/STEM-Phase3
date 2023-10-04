d1b00f43fa4470b88fe2f0ea84f9e781a52be782
Alexander Wassiljew <info@awa-consulting.de>
2023-10-04 21:42:33 +0200

    introduce constant for values

--- backend/src/main/java/uk/co/topfieldconsultancy/stem/application/AuthenticationApplication.java
+++ backend/src/main/java/uk/co/topfieldconsultancy/stem/application/AuthenticationApplication.java
@@ -5,19 +5,22 @@ import uk.co.topfieldconsultancy.stem.infrastructure.web.AuthenticationControlle
 
 @Component
 public class AuthenticationApplication {
+
+    public static final String TEST_EMAIL = "email@example.com";
+    public static final String TEST_PW = "pw";
     public boolean login(AuthenticationController.LoginRequest loginRequest) {
         //TODO: check in the database for user
-        return loginRequest.getEmail().equalsIgnoreCase("email") && loginRequest.getPassword().equalsIgnoreCase("pw");
+        return loginRequest.getEmail().equalsIgnoreCase(TEST_EMAIL) && loginRequest.getPassword().equalsIgnoreCase(TEST_PW);
     }
 
     public boolean register(AuthenticationController.RegisterRequest registerRequest) {
         //TODO: register a user
         // and send an email with a password
-        return registerRequest.getEmail().equalsIgnoreCase("email");
+        return registerRequest.getEmail().equalsIgnoreCase(TEST_EMAIL);
     }
 
     public boolean resetPassword(AuthenticationController.ResetPasswordRequest resetPassword) {
         //TODO: send an email with a password
-        return resetPassword.getEmail().equalsIgnoreCase("email");
+        return resetPassword.getEmail().equalsIgnoreCase(TEST_EMAIL);
     }
 }
