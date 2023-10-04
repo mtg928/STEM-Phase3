13af40ef515f7ee3c06d2de09034fa5069f0af39
Alexander Wassiljew <info@awa-consulting.de>
2023-10-04 19:58:45 +0200

    init backend

---
+++ backend/src/test/java/uk/co/topfieldconsultancy/stem/StemApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.topfieldconsultancy.stem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
